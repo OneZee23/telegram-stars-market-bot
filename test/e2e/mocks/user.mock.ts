bd71e2bdce73cb2286ee2f6cfa4fd2be1b34e2bc
OneZee <onezeecsgo@gmail.com>
2026-01-15 20:08:56 +0300

    Add admin user for super whitelist

--- test/e2e/mocks/user.mock.ts
+++ test/e2e/mocks/user.mock.ts
@@ -1,7 +1,9 @@
+import { ADMIN_USER_ID } from '@common/constants';
+
 /**
  * Основные тестовые пользователи
  */
-export const MAIN_TELEGRAM_USER_ID = '498124936';
+export const MAIN_TELEGRAM_USER_ID = ADMIN_USER_ID;
 export const MAIN_DEVICE_ID = 'd76a7329-e830-41c8-91c9-c149155dc3d1';
 export const MAIN_DEVICE_PLATFORM = 'android';
 
@@ -9,6 +11,6 @@ export const SECOND_TELEGRAM_USER_ID = '123456789';
 export const SECOND_DEVICE_ID = 'd76a7329-e830-41c8-91c9-c149155dc3d2';
 export const SECOND_DEVICE_PLATFORM = 'ios';
 
-export const THIRD_TELEGRAM_USER_ID = '498124936';
+export const THIRD_TELEGRAM_USER_ID = ADMIN_USER_ID;
 export const THIRD_DEVICE_ID = 'd76a7329-e830-41c8-91c9-c149155dc3d3';
 export const THIRD_DEVICE_PLATFORM = 'windows';
