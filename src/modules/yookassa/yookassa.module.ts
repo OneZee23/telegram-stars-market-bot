310547468b81786c36b7cb131d0d58688bb3f770
OneZee <onezeecsgo@gmail.com>
2026-01-25 09:17:59 +0300

    Add cron service for stucked payments

--- src/modules/yookassa/yookassa.module.ts
+++ src/modules/yookassa/yookassa.module.ts
@@ -2,13 +2,14 @@ import { FragmentModule } from '@modules/fragment/fragment.module';
 import { TelegramCoreModule } from '@modules/telegram-core/telegram-core.module';
 import { UserModule } from '@modules/user/user.module';
 import { Module } from '@nestjs/common';
+import { PaymentTimeoutService } from './services/payment-timeout.service';
 import { YooKassaService } from './services/yookassa.service';
 import { YooKassaConfig } from './yookassa.config';
 import { YooKassaController } from './yookassa.controller';
 
 @Module({
   imports: [FragmentModule, TelegramCoreModule, UserModule],
-  providers: [YooKassaConfig, YooKassaService],
+  providers: [YooKassaConfig, YooKassaService, PaymentTimeoutService],
   exports: [YooKassaService],
   controllers: [YooKassaController],
 })
